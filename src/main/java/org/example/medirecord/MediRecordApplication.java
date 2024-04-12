df10fe6abd184f835f033d8d1a707ed5125f16db
salah <aboulkacimsalah025@gmail.com>
2024-04-12 14:53:28 +0200

    Initial commit

---
+++ src/main/java/org/example/medirecord/MediRecordApplication.java
@@ -0,0 +1,13 @@
+package org.example.medirecord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediRecordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediRecordApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/medirecord/MediRecordApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.medirecord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediRecordApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
